62a45be51f14a53d903d1d2f1d126936225d1767
superjunn <superjunn00@kaist.ac.kr>
2026-04-28 20:33:15 +0900

    ui(editor): 수정 버튼 라벨을 'edit on website'로 변경

--- src/components/BlogEditor.tsx
+++ src/components/BlogEditor.tsx
@@ -251,7 +251,7 @@ export default function BlogEditor(props: { slug: string }) {
             <path d="M12 20h9"/>
             <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"/>
           </svg>
-          수정
+          edit on website
         </button>
       </Show>
 
